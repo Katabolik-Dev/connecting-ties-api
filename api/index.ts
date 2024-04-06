74177092d88f6eadc3899f5ac47982da4fbde617
Drew Kullman <agkullman@protonmail.com>
2024-04-06 12:57:59 -0800

    Added db connection

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,29 @@
 import express from "express"
+import pg from 'pg'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
+import { sql } from '@vercel/postgres'
+
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/db", async (req, res, next) => {
+    try {
+        const result = await sql`SELECT * FROM test_table`
+        res.status(200).send(result.rows)
+    } catch (error) {
+        
+    }
+
+})
+
+
 
 app.listen(5050, () => console.log("Server ready on port 3000."));
 
